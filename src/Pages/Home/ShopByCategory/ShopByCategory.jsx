ad6ebd54b1310749f4e1317539295c20fbaf6a7e
rahmanhas <rahmanhas@gmail.com>
2023-05-18 22:46:53 +0600

    shop by category section created for home page

--- src/Pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/Pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const ShopByCategory = () => {
+    const [toys,setToys] = useState([]);
+    useEffect(()=>{
+        fetch("http://localhost:5000/storedtoydata")
+        .then(res=>res.json())
+        .then(data=>setToys(data))
+    },[])
+    
     return (
         <div>
             <h1 className='text-4xl text-center font-bold my-10 mx-auto '>Buy From Your Favourite Category</h1>
